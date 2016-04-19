912d13d94cb5649f82a8efe9350ded343b96cc4e
Ondřej Tom <info@ondratom.cz>
2016-04-19 21:40:45 +0200

    VcSampleGlue refactoring
    Multiple sample files support.

--- glue/VcSampleGlue.php
+++ glue/VcSampleGlue.php
@@ -2,134 +2,143 @@
 
 namespace Totem\Tools;
 
+use ZipArchive;
+
 /**
  * Sample pieces glue.
- * 
+ *
  * @author oto
  */
 class VcSampleGlue extends VcfGlue
 {
 	/**
-	 * Name of the output file.
+	 * Result file extension.
 	 */
-	const RESULT_FILE_NAME = 'vc_sample_merge.csv';
-	
-	
+	const RESULT_FILE_EXT = '.csv';
+
 	/**
-	 * Name of the sample.
-	 * 
-	 * @var string
+	 * Result files ZIP name.
 	 */
-	protected $sample;
-	
-	
+	const ZIP_NAME = 'vc_samples';
+
+
 	/**
 	 * List of the file columns.
-	 * 
+	 *
 	 * [
 	 *		fileName => [columns],
 	 *		...
 	 * ]
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $fileColumns = [];
-	
-	
+
+
 	/**
 	 * Flag of the processed gluing.
-	 * 
+	 *
 	 * @var bool
 	 */
 	protected $isProcessed = false;
-	
-	
+
+
+	/**
+	 * Output files contents.
+	 *
+	 * @var array
+	 */
+	protected $outputFiles = [];
+
+
 	/**
 	 * Separates the input file name into sample and prefix.
-	 * 
+	 *
 	 * @param	string	$fileName	File name.
-	 * @return	array
+	 * @return	array	[sample, prefix]
 	 * @throws	GlueException
 	 */
 	protected function getFileNameParts($fileName)
 	{
-		$parts = explode('_', $fileName);
-		
+		$parts = explode('.', $fileName);
+
 		if (count($parts) < 2)
 		{
 			throw new GlueException('Invalid file name - sample hasn\'t been provided.');
 		}
-		
+
 		return [
-			array_pop($parts),
-			implode('_', $parts)
+			array_shift($parts),
+			implode('.', $parts)
 		];
 	}
-	
-	
+
+
 	/**
 	 * Adds the file row into the result.
-	 * 
+	 *
 	 * It also fills the file columns array.
-	 * 
-	 * @param	string	$name	File name.
-	 * @param	array	$row	File row.
+	 *
+	 * @param	string	$sample		Sample name.
+	 * @param	string	$name		File name.
+	 * @param	array	$row		File row.
 	 */
-	protected function addResultRow($name, array $row)
+	protected function addResultRow($sample, $name, array $row)
 	{
+		if (!isset($this->fileColumns[$sample]))
+		{
+			$this->fileColumns[$sample] = [];
+		}
+
 		if (!isset($this->fileColumns[$name]))
 		{
-			$this->fileColumns[$name] = array_keys($row);
+			$this->fileColumns[$sample][$name] = array_keys($row);
 		}
-		
-		$rowKey = $this->getRowKey($row);
-				
-		if (!isset($this->result[$rowKey]))
+
+		if (!isset($this->result[$sample]))
 		{
-			$this->result[$rowKey] = [];
+			$this->result[$sample] = [];
 		}
-		
-		$this->result[$rowKey][$name] = $row;
+
+		$rowKey = $this->getRowKey($row);
+
+		if (!isset($this->result[$sample][$rowKey]))
+		{
+			$this->result[$sample][$rowKey] = [];
+		}
+
+		$this->result[$sample][$rowKey][$name] = $row;
 	}
-	
-	
+
+
 	/**
 	 * Processes the files data.
-	 * 
+	 *
 	 * It prepares the result.
-	 * 
+	 *
 	 * @throws GlueException
 	 */
 	public function process()
-	{	
+	{
 		foreach ($this->files as $fileName => $rows)
-		{	
-			list($name, $sample) = $this->getFileNameParts($fileName);
-			
-			if (!isset($this->sample))
-			{
-				$this->sample = $sample;
-			}
-			else if ($this->sample != $sample)
-			{
-				throw new GlueException('File sample mismatch.');
-			}
-			
+		{
+			list($sample, $name) = $this->getFileNameParts($fileName);
+
 			foreach ($rows as $row)
 			{
-				$this->addResultRow($name, $row);
+				$this->addResultRow($sample, $name, $row);
 			}
 		}
-		
+
 		$this->isProcessed = true;
 	}
-	
-	
+
+
 	/**
 	 * Adds file row into the output.
-	 * 
+	 *
 	 * It's gluing file columns consecutively.
-	 * 
+	 *
 	 * @param	array	$outputRow	The result row.
 	 * @param	array	$fileRow	File row.
 	 * @param	string	$prefix		File columns prefix.
@@ -144,39 +153,40 @@ class VcSampleGlue extends VcfGlue
 			}
 		}
 	}
-	
-	
+
+
 	/**
 	 * Returns empty file columns.
-	 * 
+	 *
+	 * @param	string	$sample
 	 * @param	string	$fileName	File name.
 	 * @return	string
 	 */
-	protected function getEmptyFileRow($fileName)
+	protected function getEmptyFileRow($sample, $fileName)
 	{
 		$rows = [];
-		
-		foreach ($this->fileColumns[$fileName] as $column)
+
+		foreach ($this->fileColumns[$sample][$fileName] as $column)
 		{
 			$rows[$column] = 'N/A';
 		}
-		
+
 		return $rows;
 	}
-	
-	
+
+
 	/**
 	 * Returns position columns.
-	 * 
+	 *
 	 * Takes first file columns on position and returns its data.
-	 * 
+	 *
 	 * @param	array	$keyFiles	Files data on particular position.
 	 * @return	array
 	 */
 	protected function getKeyPositionColumns(array $keyFiles)
 	{
 		$columns = [];
-		
+
 		foreach ($keyFiles as $fileName => $fileRow)
 		{
 			foreach ($this->positionColumns as $column)
@@ -186,47 +196,145 @@ class VcSampleGlue extends VcfGlue
 
 			return $columns;
 		}
-		
+
 		return $columns;
 	}
-	
-	
+
+
 	/**
 	 * Saves the result data.
-	 * 
+	 *
 	 * Prepares the output.
-	 * 
+	 *
 	 * @throws GlueException
 	 */
 	public function save()
 	{
 		if (!$this->isProcessed)
 		{
-			throw new GlueException('Cannote save unprocessed data.');
+			throw new GlueException('Cannot save unprocessed data.');
 		}
-		
-		foreach ($this->result as $key => $files)
+
+		if (empty($this->result))
 		{
-			$outputRow = $this->getKeyPositionColumns($files);
-			
-			foreach ($this->fileColumns as $fileName => $columns)
-			{
-				if (isset($this->result[$key][$fileName]))
-				{
-					$fileRow = $this->result[$key][$fileName];
-					
-					$this->addOutputRow($outputRow, $fileRow, $fileName);
-				}
-				else
-				{
-					$this->addOutputRow($outputRow, $this->getEmptyFileRow($fileName), $fileName);
-				}
-			}
-			
-			$outputRow['VC_DETECTION_COUNT']	= count($this->result[$key]);
-			$outputRow['SAMPLE']				= $this->sample;
-			
-			$this->saveRow($outputRow);
+			throw new GlueException('Result is empty.');
 		}
+
+		foreach ($this->result as $sample => $positions)
+		{
+			$this->outputFiles[$sample] = '';
+
+			foreach ($positions as $key => $files)
+			{
+				$outputRow = $this->getKeyPositionColumns($files);
+
+				foreach ($this->fileColumns[$sample] as $fileName => $columns)
+				{
+					$fileRow = isset($this->result[$sample][$key][$fileName])
+							? $this->result[$sample][$key][$fileName]
+							: $this->getEmptyFileRow($sample, $fileName);
+
+					$this->addOutputRow(
+							$outputRow,
+							$fileRow,
+							$fileName
+					);
+				}
+
+				$outputRow['VC_DETECTION_COUNT']	= count($this->result[$sample][$key]);
+				$outputRow['SAMPLE']				= $sample;
+
+				if ($this->outputFiles[$sample] == '')
+				{
+					$this->outputFiles[$sample] .= implode("\t", array_keys($outputRow)) . "\n";
+				}
+
+				$this->outputFiles[$sample]	.= implode("\t", $outputRow) . "\n";
+			}
+		}
+	}
+
+
+	/**
+	 * Downloads single result file.
+	 *
+	 * @param	string	$fileName	Result file name.
+	 * @param	string	$content	Result file content.
+	 */
+	protected function downloadFile($fileName, $content)
+	{
+		ob_end_clean();
+
+		header('Content-type: text/plain');
+		header('Content-Disposition: attachment; filename=' . $fileName);
+
+		echo $content;
+	}
+
+
+	/**
+	 * Downloads result files in ZIP.
+	 */
+	protected function downloadZip()
+	{
+		$zipName	= tempnam(sys_get_temp_dir(), self::ZIP_NAME);
+		$tmpFiles	= [$zipName];
+		$zip		= new ZipArchive;
+
+		$zip->open($zipName, ZipArchive::CREATE);
+
+		foreach ($this->outputFiles as $sample => $content)
+		{
+			$sampleFile = tempnam(sys_get_temp_dir(), $sample);
+
+			file_put_contents($sampleFile, $content);
+
+			$tmpFiles[] = $sampleFile;
+
+			$zip->addFile($sampleFile, $sample . self::RESULT_FILE_EXT);
+		}
+
+		$zip->close();
+
+		header('Content-Type: application/zip');
+		header('Content-disposition: attachment; filename=vc_samples.zip');
+		header('Content-Length: ' . filesize($zipName));
+
+		readfile($zipName);
+
+		// Remove all created files.
+		foreach ($tmpFiles as $tmpFile)
+		{
+			unlink($tmpFile);
+		}
+	}
+
+
+	/**
+	 * Envokes the result files download.
+	 *
+	 * @throws GlueException
+	 */
+	public function download()
+	{
+		$filesCount = count($this->outputFiles);
+
+		if (!$filesCount)
+		{
+			throw new GlueException('No files for download.');
+		}
+
+		if ($filesCount == 1)
+		{
+			$sample = array_keys($this->outputFiles)[0];
+
+			$this->downloadFile($sample . self::RESULT_FILE_EXT, $this->outputFiles[$sample]);
+		}
+		else
+		{
+			$this->downloadZip();
+		}
+
+		die;
 	}
 }
